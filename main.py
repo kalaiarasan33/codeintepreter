14ee1670108007fa59d9a5af296dedfaf8d01bf9
kalaiarasan33 <48373364+kalaiarasan33@users.noreply.github.com>
2024-04-21 17:09:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -28,10 +28,36 @@ def main():
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     )
 
-    csv_agent.run("how many columns are there in file episode_info.csv")
-    csv_agent.run("print seasons ascending order of the number of episodes they have")
+    # csv_agent.run("how many columns are there in file episode_info.csv")
+    # csv_agent.run("print seasons ascending order of the number of episodes they have")
 
-   
+    grand_agent = initialize_agent(
+        tools=[
+            Tool(
+                name="PythonAgent",
+                func=python_agent_executor.run,
+                description="""useful when you need to transform natural language and write from it python and execute the python code,
+                              returning the results of the code execution,
+                            DO NOT SEND PYTHON CODE TO THIS TOOL""",
+            ),
+            Tool(
+                name="CSVAgent",
+                func=csv_agent.run,
+                description="""useful when you need to answer question over episode_info.csv file,
+                             takes an input the entire question and returns the answer after running pandas calculations""",
+            ),
+        ],
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+    )
+
+    grand_agent.run(
+        """generate and save in current working directory 15 QRcodes
+                                that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    )
+
+    grand_agent.run("print seasons ascending order of the number of episodes they have")
 
 if __name__ == "__main__":
     main()
